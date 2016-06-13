c9c9d13590c8748cbcf6614c2623834b5ffd4eed
eun-n <eunicena.12@gmail.com>
2016-06-13 01:22:48 -0700

    content edit

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.use(express.static(__dirname+'/static'));
 app.get('/', function (req, res) {
 	res.sendFile(__dirname+"/index.html");
 });
-
+app.get('/about', function (req, res) {
+	res.sendFile(__dirname+"/about.html");
+});
+app.get('/skills', function (req, res) {
+	res.sendFile(__dirname+"/skills.html");
+});
 
 app.listen(process.env.PORT || 3000);
